a4ac9fa59b2ae512e374e985b16e58f250fd3922
zidaneodrekoagnesouop <zidaneodrekoagnesouop@gmail.com>
2025-12-08 19:50:10 +0100

    replaced btc wallet with mine

--- config/paymentMethods.js
+++ config/paymentMethods.js
@@ -2,25 +2,25 @@ module.exports = [
   {
     name: "Bitcoin (BTC)",
     ticker: "BTC",
-    walletAddress: "bc1qvxvkcdw43c3utfe34d8xjl4amrarcerfxd87s2",
-    conversionRate: 0.000015312997672424355, // GBP to BTC (based on current ~£43,000/BTC)
+    walletAddress: "1JPbJBBwa4ygHsjX6jARe7g8y611ut29Mk",
+    conversionRate: 0.000014753179310141336, // GBP to BTC (based on current ~£43,000/BTC)
   },
   {
     name: "Ethereum - ERC20 (ETH)",
     ticker: "ETH",
     walletAddress: "0xb54c4B5883CC994C7c0e007F66C12b21fBcC30fb",
-    conversionRate: 0.00047397183659346964, // GBP to ETH (based on current ~£1,700/ETH)
+    conversionRate: 0.0004271715264547326, // GBP to ETH (based on current ~£1,700/ETH)
   },
   {
     name: "Litecoin (LTC)",
     ticker: "LTC",
     walletAddress: "LVASatfu1XTNvyBspTEkGG5APrddk5RFjd",
-    conversionRate: 0.01713796058269066, // GBP to LTC (based on current ~£70/LTC)
+    conversionRate: 0.015903307888040712, // GBP to LTC (based on current ~£70/LTC)
   },
   {
     name: "Tether - Tron TRC20 (USDT)",
     ticker: "USDT",
     walletAddress: "TSwtEnUuA5rFpCYHCcQXWobiSz88KxwYzB", // Example TRC20 address
-    conversionRate: 1.321332114184236, // GBP to USDT (based on current ~$1.27/£1 exchange rate)
+    conversionRate: 1.332755805817479, // GBP to USDT (based on current ~$1.27/£1 exchange rate)
   },
 ];
